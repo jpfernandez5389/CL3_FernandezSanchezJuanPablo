3d69426a9781b17a81d5e52568345ece0d809aa6
Juan Pablo Fernandez Sanchez <jpfernandez5389@gmail.com>
2023-08-17 11:04:31 -0500

    proyecto inicial de cl3

---
+++ src/main/java/pe/edu/cibertec/CL3_FernandezSanchezJuanPablo/Cl3FernandezSanchezJuanPabloApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL3_FernandezSanchezJuanPablo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl3FernandezSanchezJuanPabloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl3FernandezSanchezJuanPabloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/CL3_FernandezSanchezJuanPablo/Cl3FernandezSanchezJuanPabloApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL3_FernandezSanchezJuanPablo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl3FernandezSanchezJuanPabloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
